61249b57ad8d641569db2e4feae95aed01aae467
Elizabeth Sorenson <elso1549@colorado.edu>
2022-11-06 12:53:17 -0700

    moved code to src folder to correspond to recommended directory structure, started profile.ejs just to test that everything is working

--- Project_code/src/index.js
+++ Project_code/src/index.js
@@ -51,3 +51,7 @@ const dbConfig = {
       //set port for server to listen for client requests
       app.listen(3000);
       console.log('Server is listening on port 3000');
+
+      app.get('/profile', (req, res) =>{
+        res.render("pages/profile"); 
+      });
