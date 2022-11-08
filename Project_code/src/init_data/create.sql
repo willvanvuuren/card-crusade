1a371151975c45a4afbdb9ea418dbd93db763982
William van Vuuren <wiva8249@colorado.edu>
2022-11-07 19:21:13 -0700

    This contains the login and register page, as well as the create.sql file and some index.js routes

---
+++ Project_code/src/init_data/create.sql
@@ -0,0 +1,10 @@
+CREATE TABLE users(
+    user_id SERIAL PRIMARY KEY,
+    username VARCHAR(20) NOT NULL,
+    email VARCHAR(200) NOT NULL,
+    password VARCHAR(60) NOT NULL,
+    wins INTEGER NOT NULL,
+    losses INTEGER NOT NULL,
+    points INTEGER,
+    rank INTEGER
+);
