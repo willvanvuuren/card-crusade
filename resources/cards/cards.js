ec766158bdba69445d72b4ec69533e2e9c6d9148
jossotriv <jossotriv@gmail.com>
2022-11-13 11:36:40 -0400

    added functionality for card values to be inside the cards. Currently there is a problem with duplicate cards being left in deck1 and subsequent cards not overlaying player field'

--- resources/cards/cards.js
+++ resources/cards/cards.js
@@ -4,18 +4,20 @@ var index = 0;
 	var deck1Div = document.getElementById('Deck1');
 	var P1Score =0;
 	var P2Score =0;
-	var deck1 =[2,3,4,5,6];
+	var deck1 =[5,3,4,5,6];
 	var deck2 =[7,8,9,10,11];
-	var html1 = "<div class='col-sm' id='card1' ;'><div class='flip-box'> <div class='flip-box-inner'> <div class='flip-box-front'> <img alt='Card2' style='width:150px;height:250px'> </div> <div class='flip-box-back'> <h2 id= 'Card2Title'>Card 2</h2> <p>This is the value of the card</p> <p class='p-3 border bg-dark' id='Card2Value' style='width:10px;left:50%;text-align:center;'>2</p> </div> </div></div></div>";
-	var html2 = "<div class='col-sm' id='card2' ;'><div class='flip-box'> <div class='flip-box-inner'> <div class='flip-box-front'> <img alt='Card2' style='width:150px;height:250px'> </div> <div class='flip-box-back'> <h2 id= 'Card2Title'>Card 2</h2> <p>This is the value of the card</p> <p class='p-3 border bg-dark' id='Card2Value' style='width:10px;left:50%;text-align:center;'>2</p> </div> </div></div></div>";
+	var html1 = "<div class='col-sm' id='card1' ;'><div class='flip-box'> <div class='flip-box-inner'> <div class='flip-box-front'> <img alt='Card1' style='width:150px;height:250px'> </div> <div class='flip-box-back'> <h2 id= 'Card2Title'>Card 2</h2> <p>This is the value of the card</p> <p class='p-3 border bg-dark' id='Card2Value' style='width:10px;left:50%;text-align:center;'>2</p> </div> </div></div></div>";
+	var html2 = "<div class='col-sm' id='card2' ;'><div class='flip-box'> <div class='flip-box-inner'> <div class='flip-box-front'> <img alt='Player2' style='width:150px;height:250px'> </div> <div class='flip-box-back'> <h2 id= 'Card2Title'>Card 2</h2> <p>This is the value of the card</p> <p class='p-3 border bg-dark' id='Card2Value' style='width:10px;left:50%;text-align:center;'>2</p> </div> </div></div></div>";
+
 function generateDecks(){
 	for(let i =0;i<deck1.length;i++){
 		//copy existing card element that has been disabled, could be replaced with a partial at some point
 		const newCard= document.createElement('div');
-		newCard.innerHTML= html1;
+		let card1Value= deck1[i];
+		newCard.innerHTML= "<div class='col-sm' id='card1' ;'><div class='flip-box'> <div class='flip-box-inner'> <div class='flip-box-front'> <img alt='Player1' style='width:150px;height:250px'> </div> <div class='flip-box-back'> <h2 id= 'Card1Title'>Card 1</h2> <p>This is the value of the card</p> <p class='p-3 border bg-dark' id='Card1Value' style='width:10px;left:50%;text-align:center;'>"+card1Value+"</p> </div> </div></div></div>";
 		
 		newCard.id = '1Pcard'+i;
-		const cardValue= deck2[i];
+		
 
 		//stack em with z-index
 		newCard.style.position="absolute";
@@ -23,7 +25,8 @@ function generateDecks(){
 		newCard.style.zIndex=0-i;
 		
 		
-		console.log(newCard.id)
+		
+		console.log("card1Value: "+card1Value);
 		//have to add value from array into card
 
 
@@ -44,8 +47,7 @@ function generateDecks(){
 		newCard.style.zIndex=0-i;
 		
 		
-		console.log(newCard.id)
-		//have to add value from array into card
+	
 
 
 		document.getElementById("Deck2").appendChild(newCard);
